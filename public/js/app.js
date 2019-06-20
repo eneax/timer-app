af79a0f2feab02a89923758d79337e3e62b49800
eneax <19818937+eneax@users.noreply.github.com>
2019-06-20 17:02:21 +0200

    Create Timer component

--- public/js/app.js
+++ public/js/app.js
@@ -80,6 +80,43 @@ class EditableTimer extends React.Component {
 }
 
 
+class Timer extends React.Component {
+  render() {
+    // elapsed is in milliseconds, but we use a helper function to render it as 'HH:MM:SS'
+    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='header'>
+            {this.props.title}
+          </div>
+          <div className='meta'>
+            {this.props.project}
+          </div>
+          <div className='center aligned description'>
+            <h2>
+              {elapsedString}
+            </h2>
+          </div>
+          <div className='extra content'>
+            <span className='right floated edit icon'>
+              <i className='edit icon' />
+            </span>
+            <span className='right floated trash icon'>
+              <i className='trash icon' />
+            </span>
+          </div>
+        </div>
+        <div className='ui bottom attached blue basic button'>
+          Start
+        </div>
+      </div>
+    );
+  }
+}
+
+
 class TimerForm extends React.Component {
   render() {
     const submitText = this.props.title ? 'Update' : 'Create';
